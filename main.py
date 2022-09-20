87a782d2432382b7fc3ea07303ab387fc86fad3b
MateoArbini <mateoarbini1998@gmail.com>
2022-09-20 22:25:40 +0000

    main function

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ MAIN - Archivo de entrada a nuestro CRUD, en donde añadimos un menu que se most
 
 import os #Con esta libreria, se utilizaran las funcionalidades del sistema
 import time #Con esta libreria, se utilizaran las funcionalidades de temas fechas
-from classes.validations import Validations, check_birthday, check_email, check_name, check_phone, check_surname, check_name
+from classes.validations import Validations
 from classes.contact import Contact
-from prettytable import PrettyTable
+from prettytable import PrettyTable #Libreria para mostrar los contactos de una manera mas amigable
 from classes.dbcontacts import DBContacts
 validator = Validations()
 db = DBContacts()
@@ -56,28 +56,33 @@ def create_contact():
     '''Funcion que crea contactos en la agenda - C'''
     print('CREACIÓN DE CONTACTO')
     print('*' * 50)
-    name = check_contact_data() #Chequea el nombre con la funcion check_name() definida en validation.py
-    surname = check_contact_data() #Chequea el nombre con la funcion check_surnname() definida en validation.py
-    email = check_contact_data() #Chequea el nombre con la funcion check_email() definida en validation.py
-    phone = check_contact_data() #Chequea el nombre con la funcion check_phone() definida en validation.py
-    birthday = check_contact_data() #Chequea el nombre con la funcion check_birthday() definida en validation.py
-    contact = Contact(None, name, surname, email, phone, birthday)
-    if db.save_contact(contact):
+    name = check_contact_data('Inserta el nombre:', 'name') #Chequea el nombre con la funcion check_contact_data()
+    surname = check_contact_data('Inserta los apellidos:', 'surname') #Chequea el nombre con la funcion check_contact_data()
+    email = check_contact_data('Inserta el email:', 'email') #Chequea el nombre con la funcion check_contact_data()
+    phone = check_contact_data('Inserta el teléfono (9 cifras sin guiones ni puntos):', 'phone') #Chequea el nombre con la funcion check_contact_data()
+    birthday = check_contact_data('Inserta la fecha de nacimiento (YYYY-MM-DD):', 'birthday') #Chequea el nombre con la funcion check_contact_data()
+    #Pasamos por parametro el mensaje y el nombre del campo
+    contact = Contact(None, name, surname, email, phone, birthday) #Creamos un objeto de tipo Contact con los datos pasados y chequeados
+    if db.save_contact(contact): #En caso de que se haya guardado con exito mediante la funcion save_contact()
         print('Contacto insertado con éxito')
-    else:
+    else: #En caso de que no se haya podido guardar mediante la funcion
         print('Error al guardar el contacto')
 
 def check_contact_data(message, data_name):
+    '''Para esta funcion estamos pasando el mensaje que queremos mostrar al usuario y el nombre del campo'''
     print(message)
     input_data = input()
     try:
         getattr(validator, f'validate{data_name.capitalize()}')(input_data)
-        return input_data
+        return input_data # En este return, lo que estamos haciendo es enviar el dato a validar al validador.
     except ValueError as err:
         print(err)
         check_contact_data(message, data_name)
 
 def list_contacts():
+    '''Esta funcion se encargara de recuperar el listado de contactos, si no hay ninguno, muestra un mensaje. Si existen elementos
+       para mostrar, se crea un objeto de tipo PrettyTable, en donde en el constructor le pasamos las cabeceras y despues recorremos el
+       listado de contactos para añadirlos a la tabla.'''
     list_contacts = db.list_contacts()
     if not list_contacts:
         return print('Todavía no hay contactos guardados')
@@ -91,8 +96,8 @@ def list_contacts():
             contact.phone,
             contact.birthday
         ])
-    print(table)
-    print('Pulsa intro para salir')
+    print(table) #Printeamos la tabla
+    print('Pulsa intro para salir') #Detenemos la finalizacion de la funcion cuando el usuario presiona Enter.
     command = input()
 
 if __name__ == "__main__": # Al utilizar esto, le decimos al interprete que lo que hay que ejecutar cuando se ejecuta el main
